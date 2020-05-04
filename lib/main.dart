49906269bd6f6fbc8124bc8e39b01afe798d3d8b
oscarP <edduard.96@gmail.com>
2020-05-04 00:02:58 -0500

    Cargar el SharedPreferences mediante la clase UserPreferences antes de cargar la app

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 import 'package:preferenciasapp/src/pages/home_page.dart';
 import 'package:preferenciasapp/src/pages/settings_page.dart';
- 
-void main() => runApp(MyApp());
- 
+import 'package:preferenciasapp/src/shared_preferences/user_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final userPreferences = UserPreferences();
+  await userPreferences.initPrefs();
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,9 +19,13 @@ class MyApp extends StatelessWidget {
       title: 'Preferencias de usuario App',
       initialRoute: 'home',
       routes: {
-        'home':(BuildContext context)=>HomePage(),
-        'settings':(BuildContext context)=>SettingsPage(),
+        'home': (BuildContext context) => HomePage(),
+        'settings': (BuildContext context) => SettingsPage(),
       },
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        secondaryHeaderColor: Colors.teal,
+      ),
     );
   }
 }
